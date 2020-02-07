ac3210e1d600590d1b35eea9eaefd8887fe01da6
Jake <Raddude@users.noreply.github.com>
2020-02-07 12:11:47 -0500

    Updating main.cpp

--- H8-Robot/main.cpp
+++ H8-Robot/main.cpp
@@ -1,21 +1,103 @@
 #include <FEHLCD.h>
 #include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHIO.h>
+
+#define TURN_TIME 2
+#define MOTOR_SPEED 30
+
+FEHMotor leftDriveMotor(FEHMotor::Motor0);
+FEHMotor rightDriveMotor(FEHMotor::Motor1);
+DigitalInputPin frontRightBumper(FEHIO::P1_0);
+DigitalInputPin frontLeftBumper(FEHIO::P1_1);
+DigitalInputPin backRightBumper(FEHIO::P2_0);
+DigitalInputPin backLeftBumper(FEHIO::P2_1);
+
+
+
+void clearScreen();
+void turnLeft();
+void turnRight();
+void goStraight();
+void wait();
+void stopMotors();
+
+
+
+
+
+
+
+
+
 
 int main(void)
 {
     float x,y;
 
-    LCD.Clear(FEHLCD::Black);
-    LCD.SetFontColor(FEHLCD::White);
-    //H8isboss
-    while(true)
+
+
+
+    //Wait to start until screen is touched and released
+    while(!LCD.Touch(&x,&y))
+    {}
+    while(LCD.Touch(&x,&y))
+    {}
+
+
+
+    //Action 1
+    while(!frontRightBumper.Value() && !frontLeftBumper.Value())
     {
-        if(LCD.Touch(&x,&y))
-        {
-            LCD.WriteLine("Hello World!");
-            Sleep(100);
-        }
+        goStraight();
     }
 
+
+
+
     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+void clearScreen()
+{
+    LCD.Clear(FEHLCD::Black);
+    LCD.SetFontColor(FEHLCD::White);
+}
+
+void turnLeft()
+{
+    leftDriveMotor.SetPercent(-MOTOR_SPEED);
+    rightDriveMotor.SetPercent(MOTOR_SPEED);
+}
+
+void turnRight()
+{
+    leftDriveMotor.SetPercent(MOTOR_SPEED);
+    rightDriveMotor.SetPercent(-MOTOR_SPEED);
+}
+
+void goStraight()
+{
+    leftDriveMotor.SetPercent(MOTOR_SPEED);
+    rightDriveMotor.SetPercent(MOTOR_SPEED);
+}
+
+void wait()
+{
+    Sleep(0.5);
+}
+
+void stopMotors()
+{
+    leftDriveMotor.Stop();
+    rightDriveMotor.Stop();
 }
